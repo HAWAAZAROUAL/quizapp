28937018401082e3adaede6c73dcbbe542a1a9ec
Brian Sohn <kolpp15@gmail.com>
2021-08-26 14:59:02 -0700

    create quiz on click is now wokring

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,74 +1,52 @@
 // HELPER FUNCTIONS FOR APP.POST
 // FXNS FOR BUTTONS
 
-// CREATE QUIZ BUTTON (router.post),ADD INPUTTED QUESTION AND ANSWER TO DATABASE,
-// INSERT INTO quizzes (user_id,title,is_private) VALUES ()
-// INSERT INTO questions (quizid, question) VALUES ()
-// INSERT INTO answers (questionid, answer, is_right) VALUES()
-
-// PUBLIC/PRIVATE BUTTONS (router.post),
-// IS_RIGHT SELECTION (router.post)
-
-// START QUIZ BUTTON (router.post), ON THE HOMEPAGE FOR PUBLIC, AND ON MYQUIZZES,
-// $('.start-quiz').on("click", function() {
-//   event.preventDefault();
-
-// ANSWERS BUTTON: FOR EACH QUESTION WHEN A PERSON STARTS THE QUIZ ,
-// make a fxn where if the radiobutton is clicked- it evaluates to true or false depending.
-
-//  function () => {
-//   if ("is_right".on("click"is_right) {
-
-//   }
-
-//  };
-// DELETE BUTTON: ON MYQUIZZES PAGE- DELETE FROM DATABASE,
-
-// $(".delete-my-quiz").on("click", function() {
-
-//   db.query(`
-//   DELETE FROM quizzes WHERE quizzes.id=$1
-//   `, []
-// });
-// app.post("/myquiz/:id/delete", (req, res) => {
-//   delete data.rows[req.params.quiz_id];
-//   res.redirect(`/myquiz/${req.session.user_id}`);
-// });
-
-
-
-
 $(document).ready(function() {
-
+  let clickCount=0;
   // ADD QUESTION
   $("#add-question").on("click", function(event) {
     event.preventDefault();
+    clickCount ++;
     const $createquiz = $('#New_question_container');
     $createquiz.empty();
+    const radios= [];
+    const answers= [];
+    const questions = [];
+    const isRight =[];
+
+    for(let i=1;i<=4;i++) {
+      radios.push("a"+(clickCount*4+i));
+      answers.push("answer"+(clickCount*4+i));
+    }
+    questions.push("questionName"+(clickCount+1));
+    isRight.push("is_right"+(clickCount+1));
 
     let $newQuestion = `<h2 class="quiz-question"> Write your question here</h2>
-    <div class="write-question">
-      <input type="text" name="option" placeholder="Type your question here">
-    <!-- each div has a possible answer-->
-    <h2 class="quiz-question"> Write the answers here</h2>
-    <p> Check off the correct answer</p>
+   <div class="write-question">
+     <input type="text" name=${questions[0]} placeholder="Type your question here">
+   <!-- each div has a possible answer-->
+   <h2 class="quiz-question"> Write the answers here</h2>
+   <p> Check off the correct answer</p>
 
-    <form id="answers-form">
 
-      <div class="possible-answer-container">
-        <label for="answer1">Answer1</label>
-        <input type="text"></input><input id="a5" class="answer" type="radio" name="is_right"></input><br>
-        <label for="answer1">Answer2</label>
-        <input type="text"></input><input id="a6" class="answer" type="radio" name="is_right"></input><br>
-        <label for="answer1">Answer3</label>
-        <input type="text"></input><input id="a7" class="answer" type="radio" name="is_right"></input><br>
-        <label for="answer1">Answer4</label>
-        <input type="text"></input><input id="a8" class="answer" type="radio" name="is_right"></input><br>
-      </div>
-      <div class="create-quiz-container"></div>
-      </form>`;
+
+     <div class="possible-answer-container">
+       <label for="answer1">${answers[0]}</label>
+       <input type="text" name=${answers[0]}></input><input id=${radios[0]} type="radio" name=${isRight} value=${radios[0]}  ></input><br>
+       <label for="answer1">${answers[1]}</label>
+       <input type="text" name=${answers[1]}></input><input id=${radios[1]} type="radio" name=${isRight} value=${radios[1]}  ></input><br>
+       <label for="answer1">${answers[2]}</label>
+       <input type="text" name=${answers[2]}></input><input id=${radios[2]}   type="radio" name=${isRight} value=${radios[2]}  ></input><br>
+       <label for="answer1">${answers[3]}</label>
+       <input type="text" name=${answers[3]}></input><input id=${radios[3]} type="radio" name=${isRight} value=${radios[3]} ></input><br>
+     </div>
+     `;
     $('#create-quiz-container').append($newQuestion);
 
   });
-
 });
+
+
+
+
+
--- routes/createQuiz.js
+++ routes/createQuiz.js
@@ -7,12 +7,15 @@ module.exports = function(db) {
   // create/:user_id
   router.get("/", (req, res) => {
 
+    const userId = req.session.user_id;
+    console.log('THIS IS SESSION UESRID', userId);
+
     db.query(`
       SELECT users.name, users.id
       FROM users;
     `)
       .then((user) => {
-        let templateVars = { userName: user.rows[0].name, userId: user.rows[0].id };
+        let templateVars = { userName: user.rows[0].name, userId: userId };
         res.render("create_quiz", templateVars);
 
 
@@ -22,41 +25,80 @@ module.exports = function(db) {
       });
   });
 
-  // should be /create --- put userid in the BODY - check source of userId
+
+
+
   router.post('/', (req, res) => {
+    const userId   = req.session.user_id; //MUST LOGIN FIRST
+    const quizName = req.body["quizName"];
+    let isPrivate  = req.body["is_private"];
+    const numberOfQuestions = Math.floor(Object.keys(req.body).length / 6);
+    const questionName = [];
+    const answers      = [];
+    let answer        = [];
+    const isRight     = [];
+    let isRight1     = [];
 
-    const quizName     = req.body["quizName"];
-    const questionName = req.body["questionName"];
-    const answer1      = req.body["answer1"];
-    const answer2      = req.body["answer2"];
-    const answer3      = req.body["answer3"];
-    const answer4      = req.body["answer4"];
-    const userId       = req.body["user_id"]; // DOUBLE CHECK
-    let  isPrivate     = req.body["is_private"];
-    let isRight        = req.body["is_right"];
-
-    //IF radio checked, is_private
-    if (isPrivate) {
+    isPrivate = false;
+    if (isPrivate.checked) {
       isPrivate = true;
     }
-    //IF radio checked, is_right (correct answer)
-    if (isRight) {
-      isRight = true;
+    console.log('THIS IS THE BODY', req.body);
+
+    //loop through all the input field from html,add the contents to Arrays,then insert into tables
+    for (let i = 0; i < numberOfQuestions; i++) {
+      let strQuestionMame = req.body["questionName" + (i + 1)];
+      let strIsRight = req.body["is_right" + (i + 1)];
+      questionName.push(strQuestionMame);//questionName Array
+      for (let j = 0; j < 4; j++) {
+        let strAnswer = req.body["answer" + (4 * i + (j + 1))];
+        answer.push(strAnswer);        //answer Array
+        let strIsRightId = ("a" + (4 * i + (j + 1)));
+        console.log(strIsRight, strIsRightId);
+        if (strIsRight === strIsRightId) {
+          isRight1.push(true);
+        } else {
+          isRight1.push(false);
+        }
+      }
+      isRight[i] = isRight1;
+      answers[i] = answer;
+      isRight1 = [];
+      answer = [];
     }
-    db.query(`INSERT INTO quizzes( user_id ,title, is_private) VALUES ($1,$2,$3) returning id;`,[userId, quizName,isPrivate])
+    console.log(answers, isRight);
+    //insert quizName to quizzes table
+    db.query(`INSERT INTO quizzes( user_id ,title, is_private) VALUES ($1,$2,$3) returning id;`, [userId, quizName, isPrivate])
       .then(data => {
+        //get quizId from quiz
+        // console.log("THIS IS DATA", data.rows[0]);
         const quizId = data.rows[0].id;
-        db.query(`INSERT INTO questions(quiz_id,question) VALUES($1,$2) returning id;`,[`${quizId}`,questionName])
-          .then(data => {
-            const questionId = data.rows[0].id;
-            db.query(` INSERT INTO answers(question_id,answer,is_right)   VALUES($1,$2,$3),($4,$5,$6),($7,$8,$9),($10,$11,$12)   returning *;`,[`${questionId}`,answer1,isRight,`${questionId}`,answer2,isRight,`${questionId}`,answer3,isRight,`${questionId}`,answer4,isRight])
-              .then(data => {
-                res.redirect(`/myquiz/${userId}`);
-              });
-          });
+        for (let i = 0; i < numberOfQuestions; i++) {
+
+          createQuiz(userId, quizId, questionName[i], answers[i], isRight[i], res);
+        }
       });
   });
 
+  const createQuiz = function(userId, quizId, questionName, answers, isRight, res) {
+    db.query(`INSERT INTO questions(quiz_id,question) VALUES($1,$2) returning id;`, [`${quizId}`, questionName])
+      .then(data => {
+        const questionId = data.rows[0].id;
+
+        db.query(` INSERT INTO answers(question_id,answer,is_right) VALUES($1,$2,$3),($1,$4,$5),($1,$6,$7),($1,$8,$9)   returning *;`, [`${questionId}`, answers[0], isRight[0], answers[1], isRight[1], answers[2], isRight[2], answers[3], isRight[3]])
+          .then(data => {
+            res.redirect(`/myquiz/${userId}`);
+          });
+      });
+  };
+
+
+
+
+
+
+
   return router;
 };
+
 
